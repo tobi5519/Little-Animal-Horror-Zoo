6a40500f27d79032475264ed37ce8d32d1076aa5
Tobias KT <tobi5519@stud.kea.dk>
2017-02-16 17:44:47 +0100

    randomize code

--- Animals.java
+++ Animals.java
@@ -1,17 +1,21 @@
+import java.util.Random;
 public class Animals {
     private  String name;
-    private int x;
-    private int y;
+    private int x = 0;
+    private int y = 0;
 
     public void move(){
     // Randomize increments of x or y value
     }
     public void setLocation(){
     // Randomize x & y values between 1 og 10
-    // 
+    Random rand = new Random();
+    this.x = rand.nextInt(10) + 1;
+    this.y = rand.nextInt(10) + 1;
+
     }
-    public int getLocation(){
-        return x + y;
+    public String getLocation(){
+        return x + " " + y;
     }
 
     public void printLocation(){
--- Board.java
+++ Board.java
@@ -1,11 +1,14 @@
 public class Board {
      public static void main(String[] args){
      
-     Animals snake = new Animals();
+    //  Animals snake = new Animals();
      Animals rabbit = new Animals();
      
-     System.out.println(snake.getLocation());
+     rabbit.setLocation();
+     
+    //  System.out.println(snake.getLocation());
      System.out.println(rabbit.getLocation());
+
 
      }
 }
