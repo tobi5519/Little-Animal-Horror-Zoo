47d8bf0731f488d2090e691b142ae647be28579a
Tobias KT <tobi5519@stud.kea.dk>
2017-02-16 16:00:44 +0100

    init

---
+++ Animals.java
@@ -0,0 +1,21 @@
+public class Animals {
+    private  String name;
+    private int x;
+    private int y;
+
+    public void move(){
+    // Randomize increments of x or y value
+    }
+    public void setLocation(){
+    // Randomize x & y values between 1 og 10
+    // 
+    }
+    public int getLocation(){
+        return x + y;
+    }
+
+    public void printLocation(){
+        System.out.println("I am location " + x + " " + y);
+    }
+
+}
---
+++ Board.java
@@ -0,0 +1,11 @@
+public class Board {
+     public static void main(String[] args){
+     
+     Animals snake = new Animals();
+     Animals rabbit = new Animals();
+     
+     System.out.println(snake.getLocation());
+     System.out.println(rabbit.getLocation());
+
+     }
+}
