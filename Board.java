68acdcb9ec01a5bef3b88263cd09c00ee59a8df8
Tobias KT <tobi5519@stud.kea.dk>
2017-02-20 11:00:32 +0100

    added fight()

--- Board.java
+++ Board.java
@@ -4,22 +4,22 @@ public class Board {
     // We make the animals
     Animals snake = new Animals();
     Animals rabbit = new Animals();
-
     
     // We place the animals in a 10x10
-    rabbit.setLocation(3,5);
-    snake.setLocation(8,4);
+    rabbit.setLocation(03,03);
+    snake.setLocation(03,03);
 
     // Yo, where you at?!
     System.out.println("Snake is at " + snake.getLocation());
     System.out.println("Rabbit is at " + rabbit.getLocation());
 
-    // The animals move on the board (10x10)
-    snake.move(1,0);
-    rabbit.move(0,1);
-    System.out.println("Snake is at " + snake.getLocation());
-    System.out.println("Rabbit is at " + rabbit.getLocation());
 
-    
+
+    fight(snake, rabbit);
      }
+    public static void fight(Animals snake, Animals rabbit){
+        if (snake.getLocation().equals(rabbit.getLocation())){
+            System.out.println("Game over!");
+        } 
+    }
 }
