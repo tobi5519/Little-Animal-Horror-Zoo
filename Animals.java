99923d7b491cf2d1129fe936d2c8347b9d673637
Tobias KT <tobi5519@stud.kea.dk>
2017-02-20 09:45:21 +0100

    fixed getLocation

--- Animals.java
+++ Animals.java
@@ -4,18 +4,39 @@ public class Animals {
     private int x = 0;
     private int y = 0;
 
+    public int getRandom(){
+        Random rand = new Random();
+        return rand.nextInt(1) + 1;
+    }
+
     public void move(){
     // Randomize increments of x or y value
+    Random rand = new Random();
+    this.x = this.x + rand.nextInt(1) + 1;
+    this.y = this.y + rand.nextInt(1) + 1;
     }
     public void setLocation(){
     // Randomize x & y values between 1 og 10
     Random rand = new Random();
     this.x = rand.nextInt(10) + 1;
     this.y = rand.nextInt(10) + 1;
-
     }
     public String getLocation(){
-        return x + " " + y;
+        String newX = "";
+        String newY = ""; 
+        if (x < 10){
+            newX = String.format("0%d", x);
+        } else {
+            newX = "" + x;
+        }
+        if (y < 10){
+            newY = String.format("0%d", y);
+        } else {
+            newY = "" + y;
+        }
+
+        return "" + newX + ", " + newY;
+
     }
 
     public void printLocation(){
--- Board.java
+++ Board.java
@@ -1,14 +1,21 @@
 public class Board {
-     public static void main(String[] args){
-     
-    //  Animals snake = new Animals();
-     Animals rabbit = new Animals();
-     
-     rabbit.setLocation();
-     
-    //  System.out.println(snake.getLocation());
-     System.out.println(rabbit.getLocation());
+    public static void main(String[] args){
+    
+    // We make the animals
+    Animals snake = new Animals();
+    Animals rabbit = new Animals();
+    
+    // We place the animals in a 10x10
+    rabbit.setLocation();
+    snake.setLocation();
 
+    // Yo, where you at?!
+    System.out.println("Snake is at " + snake.getLocation());
+    System.out.println("Rabbit is at " + rabbit.getLocation());
+
+    // The animals move on the board (10x10)
+    snake.move();
+    rabbit.move();
 
      }
 }
