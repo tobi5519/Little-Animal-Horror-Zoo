de63cf29736b72eb8f74755d5a9b12d1c5a7e204
Fedepede <Peterthemmingsen@gmail.com>
2017-02-20 12:30:39 +0100

    done deal

--- Board.java
+++ Board.java
@@ -6,22 +6,40 @@ public class Board {
     Animals rabbit = new Animals();
     
     // We place the animals in a 10x10
-    rabbit.setLocation(3,3);
-    snake.setLocation(3,3);
-
+    rabbit.setLocation(5,6);
+    snake.setLocation(5,2);
+    snake.move(0,1);
+    rabbit.move(0,-2);
+    dialog(snake, rabbit);
+    rabbit.move(0,-1);
+    
+  
     // Yo, where you at?!
     // System.out.println("Snake is at " + snake.getLocation());
     // System.out.println("Rabbit is at " + rabbit.getLocation());
 
+    System.out.println(snake.getLocation("x") + ", " + snake.getLocation("y"));
+    System.out.println(rabbit.getLocation("x") + ", " + rabbit.getLocation("y"));
 
     fight(snake, rabbit);
      }
     public static void fight(Animals snake, Animals rabbit){
-        if (snake.getLocation("x") == (rabbit.getLocation("x")) &&
-            snake.getLocation("y") == (rabbit.getLocation("y")) ){
+        if (snake.getLocation("x") == rabbit.getLocation("x") &&
+            snake.getLocation("y") == rabbit.getLocation("y") ){
             System.out.println("Game over!");
         } 
 
-    System.out.println(snake.getLocation("x") + ", " + snake.getLocation("y"));
+   
+    }
+    public static void dialog(Animals snake, Animals rabbit){
+
+        if (snake.getLocation("x") == rabbit.getLocation("x") -1 ||
+            snake.getLocation("x") == rabbit.getLocation("x") +1 ||
+            snake.getLocation("y") == rabbit.getLocation("y") -1 ||
+            snake.getLocation("y") == rabbit.getLocation("y") +1 ){
+            System.out.println("hej med dig");
+        } else{
+            System.out.println("mums");
+        }
     }
 }
