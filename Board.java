858dbbcc61e0f134b084c021d72a8457e90a6031
Tobias KT <tobi5519@stud.kea.dk>
2017-02-23 12:21:51 +0100

    Changed Animals class to GamePiece

--- Board.java
+++ Board.java
@@ -2,19 +2,24 @@ public class Board {
     public static void main(String[] args){
     
     // We make the animals
-    Animals snake = new Animals();
-    Animals rabbit = new Animals();
+    GamePiece snake = new GamePiece();
+    GamePiece rabbit = new GamePiece();
     
     // We place the animals in a 10x10
     rabbit.setLocation(5,6);
     snake.setLocation(5,2);
-    // Let the game begin! :P
-    boolean gameOver = false;
-    while (!gameOver){
-        
-        
-        
-    }
+    // // Let the game begin! :P
+    // boolean gameOver = false;
+    // while (!gameOver){
+    //     fight testtest = new fight();
+
+
+    //     if (testtest = true){
+    //         break;
+    //     }
+    // }
+
+
     // snake.move(0,1);
     // rabbit.move(0,-2);
     // dialog(snake, rabbit);
@@ -26,21 +31,7 @@ public class Board {
 
     
      }
-    public static void fight(Animals snake, Animals rabbit){
-        if (snake.getLocation("x") == rabbit.getLocation("x") &&
-            snake.getLocation("y") == rabbit.getLocation("y") ){
-            System.out.println("Game over!");
-        } 
-    }
+    
         
-    public static void dialog(Animals snake, Animals rabbit){
-
-        if (snake.getLocation("x") == rabbit.getLocation("x") -1 ||
-            snake.getLocation("x") == rabbit.getLocation("x") +1 ||
-            snake.getLocation("y") == rabbit.getLocation("y") -1 ||
-            snake.getLocation("y") == rabbit.getLocation("y") +1 ){
-        } else{
-            System.out.println("Yo, where you at?");
-        }
-    }
+   
 }
