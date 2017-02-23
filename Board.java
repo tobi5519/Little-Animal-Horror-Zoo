847e58de8917f9b4c0a2540d7967c67539c56e51
Tobias KT <tobi5519@stud.kea.dk>
2017-02-23 11:36:10 +0100

    game while loop

--- Board.java
+++ Board.java
@@ -8,38 +8,39 @@ public class Board {
     // We place the animals in a 10x10
     rabbit.setLocation(5,6);
     snake.setLocation(5,2);
-    snake.move(0,1);
-    rabbit.move(0,-2);
-    dialog(snake, rabbit);
-    rabbit.move(0,-1);
+    // Let the game begin! :P
+    boolean gameOver = false;
+    while (!gameOver){
+        
+        
+        
+    }
+    // snake.move(0,1);
+    // rabbit.move(0,-2);
+    // dialog(snake, rabbit);
+    // rabbit.move(0,-1);
     
-  
-    // Yo, where you at?!
-    // System.out.println("Snake is at " + snake.getLocation());
-    // System.out.println("Rabbit is at " + rabbit.getLocation());
 
     System.out.println(snake.getLocation("x") + ", " + snake.getLocation("y"));
     System.out.println(rabbit.getLocation("x") + ", " + rabbit.getLocation("y"));
 
-    fight(snake, rabbit);
+    
      }
     public static void fight(Animals snake, Animals rabbit){
         if (snake.getLocation("x") == rabbit.getLocation("x") &&
             snake.getLocation("y") == rabbit.getLocation("y") ){
             System.out.println("Game over!");
         } 
-
-   
     }
+        
     public static void dialog(Animals snake, Animals rabbit){
 
         if (snake.getLocation("x") == rabbit.getLocation("x") -1 ||
             snake.getLocation("x") == rabbit.getLocation("x") +1 ||
             snake.getLocation("y") == rabbit.getLocation("y") -1 ||
             snake.getLocation("y") == rabbit.getLocation("y") +1 ){
-            System.out.println("hej med dig");
         } else{
-            System.out.println("mums");
+            System.out.println("Yo, where you at?");
         }
     }
 }
